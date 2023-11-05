4bd32a74c44ca1819e72a7bdf6c775ff4ac95c73
ttybitnik <vinicius.moraes@eternodevir.com>
2023-11-04 22:33:36 -0300

    feat(c): add 1.01 solution

---
+++ c/1.01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void tty_find_last(char list[], size_t length)
+{
+	char last = list[length - 1];
+
+	if (last == 0) {
+		printf("The list is empty.\n");
+	} else {
+		printf("%c\n", last);
+	}
+}
+
+int main()
+{
+	char list[] = {'a', 'b', 'c', 'd'};
+	size_t length = sizeof(list) / sizeof(list[0]);
+
+	tty_find_last(list, length);
+	return 0;
+}
+/* => d  */
