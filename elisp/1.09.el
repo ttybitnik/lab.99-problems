8270fa082d404a73e2ce9cb55713d481a7a9f992
Vinícius Moraes <vinicius.moraes@eternodevir.com>
2024-04-18 09:20:26 -0300

    feat(elisp): add 1.09 alternative solution

--- elisp/1.09.el
+++ elisp/1.09.el
@@ -1,12 +1,24 @@
 (defun tty/pack (list)
   (let (pack)
     (while (consp list)
-      (setq item (pop list))
-      (let ((nest (list item)))
+      (let* ((item (pop list))
+	     (nest (list item)))
         (while (equal item (car list))
           (push (pop list) nest))
-        (push (nreverse nest) pack)))
+        (push nest pack)))
     (nreverse pack)))
 
 (tty/pack '(a a a a b c c a a d e e e e))
+;; => ((a a a a) (b) (c c) (a a) (d) (e e e e))
+
+(defun tty/pack-rec (list)
+  (if (eql list nil)
+      nil
+    (let* ((item (pop list))
+	   (nest (list item)))
+      (while (equal item (car list))
+	(push (pop list) nest))
+      (cons nest (tty/pack-rec list)))))
+
+(tty/pack-rec '(a a a a b c c a a d e e e e))
 ;; => ((a a a a) (b) (c c) (a a) (d) (e e e e))
