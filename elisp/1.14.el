9ad230735aee6f73c2f6f73a8de7ddb0e9c638d8
Vinícius Moraes <vinicius.moraes@eternodevir.com>
2024-04-18 21:25:42 -0300

    feat(elisp): add 1.14 alternative solution

--- elisp/1.14.el
+++ elisp/1.14.el
@@ -8,3 +8,12 @@
 
 (tty/duplicate '(a b c c d))
 ;; => (a a b b c c c c d d)
+
+(defun tty/duplicate-dolist (list)
+  (let (dup)
+    (dolist (item list (nreverse dup))
+      (push item dup)
+      (push item dup))))
+
+(tty/duplicate-dolist '(a b c c d))
+;; => (a a b b c c c c d d)
