ad3e76add9da929cd292bf20ba55b45712e6da59
Vinícius Moraes <vinicius.moraes@eternodevir.com>
2024-04-19 22:30:24 -0300

    feat(elisp): add 1.17 alternative solution

--- elisp/1.17.el
+++ elisp/1.17.el
@@ -7,3 +7,16 @@
 
 (tty/split-intotwo '(a b c d e f g h i k) 3)
 ;; => ((a b c) (d e f g h i k))
+
+(defun tty/split-intotwo-ptr (list n)
+  (let ((l1 (nthcdr (- n 1) list))
+	(l2 (copy-sequence (nthcdr n list))))
+    (cond ((and (> n 0)
+		(< n (length list)))
+	   (setq l1 (nconc list (setcdr l1 nil)))
+	   (list l1 l2))
+	  (t
+	   nil))))
+
+(tty/split-intotwo-ptr '(a b c d e f g h i k) 3)
+;; => ((a b c) (d e f g h i k))
